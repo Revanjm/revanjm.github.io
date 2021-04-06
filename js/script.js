2db8644f368cf6a99c2d2e859dcfdd076ccd7533
Pavel Pashuto <revanjm.93@gmail.com>
2021-04-06 13:04:14 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active')
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active')
+});
+$(function(){
+    $("a[href^='#']").click(function(){
+            var _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+    });
+});
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1000) {
+        $('.page_up').fadeIn('FAST');
+    } else {
+        $('.page_up').fadeOut('FAST');
+    }
+  });
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+    });
+    return false;
+  });
---
+++ mailer/smart.php
@@ -0,0 +1,45 @@
+<?php 
+
+$name = $_POST['name'];
+$text = $_POST['text'];
+$email = $_POST['email'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'revanjm.93@mail.ru';                 // Наш логин
+$mail->Password = 'n23a09i93djm23
+';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('revanjm.93@mail.ru', 'Portfolior');   // От кого письмо 
+$mail->addAddress('revanjm.93@gmail.com');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	Сообщение: ' . $text . '<br>
+	E-mail: ' . $email . '';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
